9428a4321b5a05cf3772f27e52265dce949afbef
sampete1997 <sohelpatel620@gmail.com>
2022-06-10 11:12:07 +0530

    correctino in env and webappconfig

--- src/webApiConfig/index.js
+++ src/webApiConfig/index.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 const axiosConn = axios.create();
 
-axiosConn.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axiosConn.defaults.baseURL = process.env.REACT_APP_URL;
 
 export default axiosConn;
